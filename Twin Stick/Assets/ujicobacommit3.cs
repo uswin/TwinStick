2d27ab299a97a44f33503b88fe44f8dba0d6a320
aswin <woodenboxlwp@gmail.com>
2016-07-03 21:40:35 +0700

    commit 3

---
+++ Twin Stick/Assets/ujicobacommit3.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ujicobacommit3 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("commit 3");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
