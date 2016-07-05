616c68117dbe1a441d9b3f4e05d23f894b98e656
aswin <woodenboxlwp@gmail.com>
2016-07-05 15:51:40 +0700

    Set Selfie Stick and put camera on stick, Camera Pan with Right analgo stick (twin stick scheme)

---
+++ Twin Stick/Assets/CameraPan.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPan : MonoBehaviour {
+
+	public Transform myTarget;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	void LateUpdate()
+	{
+		if(myTarget.tag=="Player")
+		{
+			this.transform.LookAt(myTarget);
+		}
+	}
+}
---
+++ Twin Stick/Assets/SelfieStick.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelfieStick : MonoBehaviour {
+
+	public float panSpeed=1f;
+
+	private GameObject myPlayer;
+	private Vector3 armRotation;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myPlayer = GameObject.FindGameObjectWithTag("Player");
+		armRotation = this.transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		armRotation.y += Input.GetAxis("RHor")*panSpeed;
+		armRotation.x += Input.GetAxis("RVer")*panSpeed;
+		this.transform.position = myPlayer.transform.position;
+		this.transform.rotation = Quaternion.Euler(armRotation);
+	}
+}
